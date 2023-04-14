a434e6e2ab0205c6ae275cdd158445e4d7cedc42
Theo Paris <theop@flowtr.dev>
2023-04-14 01:42:39 -0700

    fix: update to the new zig build api

--- build.zig
+++ build.zig
@@ -14,7 +14,7 @@ pub fn build(b: *std.build.Builder) void {
     });
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&b.addRunArtifact(main_tests).step);
 
     const calculator_example = b.addExecutable(.{
         .root_source_file = .{ .path = "examples/calculator.zig" },
@@ -22,11 +22,10 @@ pub fn build(b: *std.build.Builder) void {
         .optimize = optimize,
     });
 
-    calculator_example.install();
+    b.installArtifact(calculator_example);
     calculator_example.addAnonymousModule("parser-toolkit", .{
         .source_file = .{ .path = "src/main.zig" },
     });
 
-    const runner = calculator_example.run();
-    b.step("run", "Runs the calculator example").dependOn(&runner.step);
+    b.step("run", "Runs the calculator example").dependOn(&b.addRunArtifact(calculator_example).step);
 }
