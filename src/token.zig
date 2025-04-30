0d5e00d4830bca53d10fee5d562152f1329d5784
Felix "xq" Queißner <git@random-projects.net>
2025-04-30 19:23:15 +0200

    Adds formatting to token

--- src/token.zig
+++ src/token.zig
@@ -14,5 +14,16 @@ pub fn Token(comptime Type: type) type {
 
         /// The type of the token that was matched by a matching function
         type: Type,
+
+        pub fn format(tok: @This(), fmt: []const u8, opt: std.fmt.FormatOptions, writer: anytype) !void {
+            _ = fmt;
+            _ = opt;
+
+            try writer.print("Token(type={}, text=\"{}\", location={})", .{
+                std.zig.fmtId(@tagName(tok.type)),
+                std.zig.fmtEscapes(tok.text),
+                tok.location,
+            });
+        }
     };
 }
