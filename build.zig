ae752b43fa7c374878d6ecb70eb7426af6aba9a6
Linus Groh <mail@linusgroh.de>
2025-07-19 17:41:34 +0100

    Update for std.Build API changes

--- build.zig
+++ build.zig
@@ -6,21 +6,24 @@ pub fn build(b: *std.Build) void {
 
     const module = b.addModule("parser-toolkit", .{
         .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
     const main_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .optimize = optimize,
+        .root_module = module,
     });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&b.addRunArtifact(main_tests).step);
 
     const calculator_example = b.addExecutable(.{
-        .root_source_file = b.path("examples/calculator.zig"),
         .name = "calculator",
-        .optimize = optimize,
-        .target = target,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("examples/calculator.zig"),
+            .optimize = optimize,
+            .target = target,
+        }),
     });
     calculator_example.root_module.addImport("parser-toolkit", module);
     b.installArtifact(calculator_example);
